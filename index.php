d6d202b9854bf5b32f26b14cf58f63a51868e7df
Brendita... martinez <brendita_mtz@live.com.mx>
2020-06-11 15:16:55 -0500

    cambio a modelo

--- index.php
+++ index.php
@@ -15,12 +15,15 @@
       embedded
       arjs="sourceType: webcam; debugUIEnabled: false;"
     >
-      <a-text
+    <a-entity gltf-model="./assets/magnemite/scene.gltf" 
+        rotation="0 180 0" scale="0.15 0.15 0.15" 
+        gps-entity-place="longitude: 18.617806; latitude: -100.907436;" animation-mixer/>
+      <!-- <a-text
         value="TE AMO MUCHO"
         look-at="[gps-camera]"
         scale="15 15 15"
         gps-entity-place="latitude: 18.617806; longitude: -100.907436;"
-      ></a-text>
+      ></a-text> -->
       <a-camera gps-camera rotation-reader> </a-camera>
     </a-scene>
   </body>
