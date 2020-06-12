4b9375e643fd95d3b87fc7642a52489a5551e1a7
Brendita... martinez <brendita_mtz@live.com.mx>
2020-06-12 10:35:50 -0500

    mod position

--- index.php
+++ index.php
@@ -5,17 +5,14 @@
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>GeoAR.js demo</title>
-    <!-- <script src='https://aframe.io/releases/0.9.2/aframe.min.js'></script>
+    <script src='https://aframe.io/releases/0.9.2/aframe.min.js'></script>
     <script src="https://raw.githack.com/jeromeetienne/AR.js/master/aframe/build/aframe-ar.min.js"></script>
     <script src="https://raw.githack.com/donmccurdy/aframe-extras/master/dist/aframe-extras.loaders.min.js"></script>
     <script>
         THREEx.ArToolkitContext.baseURL = 'https://raw.githack.com/jeromeetienne/ar.js/master/three.js/'
-    </script> -->
+    </script>
     <!-- <script src="./script.js"></script> -->
     <!-- <link rel="stylesheet" type="text/css" href="./style.css"/> -->
-    <script src="https://aframe.io/releases/1.0.3/aframe.min.js"></script>
-    <script src="https://unpkg.com/aframe-extras@3.3.0/dist/aframe-extras.min.js"></script>
-    <script src="https://unpkg.com/aframe-environment-component@1.1.0/dist/aframe-environment-component.min.js"></script>
 </head>
 
 <body style='margin: 0; overflow: hidden;'>
@@ -24,10 +21,11 @@
         vr-mode-ui='enabled: false' 
         embedded
         arjs='sourceType: webcam; sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960; debugUIEnabled: false;'>
-        <a-entity position="0 2.15 0" rotation="0 55 0" gltf-model="./assets/magnemite/scene.gltf" scale="0.5 0.5 0.5" crossOrigin="anonymous" 
-         animation-mixer
-         >
+        <a-entity gltf-model="./assets/magnemite/scene.gltf" scale="0.5 0.5 0.5" crossOrigin="anonymous">
         </a-entity>
-    <a-camera position="0 1.2 0" ></a-camera>
+    <a-camera position="0 1.2 0" rotation-reader></a-camera>
 </a-scene>
+<div class="centered">
+    <button data-action="change"></button>
+</div>
 </body>
