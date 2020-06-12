2aeada1362b1be4ce66775e5caa657996206382d
Brendita... martinez <brendita_mtz@live.com.mx>
2020-06-12 10:32:23 -0500

    mod position

--- index.php
+++ index.php
@@ -26,11 +26,8 @@
         arjs='sourceType: webcam; sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960; debugUIEnabled: false;'>
         <a-entity position="0 2.15 0" rotation="0 55 0" gltf-model="./assets/magnemite/scene.gltf" scale="0.5 0.5 0.5" crossOrigin="anonymous" 
          animation-mixer
-         shadow="cast: true; receive: false">
+         >
         </a-entity>
     <a-camera position="0 1.2 0" ></a-camera>
 </a-scene>
-<div class="centered">
-    <button data-action="change"></button>
-</div>
 </body>
